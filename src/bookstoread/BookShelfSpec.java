2d3472c6d149d024cb12f3f9223dda6a644b6acb
yasmine1110 <yasminemariamd@gmail.com>
2026-06-08 11:21:40 +0000

    suppression de la  methode nested

--- src/bookstoread/BookShelfSpec.java
+++ src/bookstoread/BookShelfSpec.java
@@ -137,38 +137,11 @@ public class BookShelfSpec {
         assertThat(booksByAuthor).containsKey("Frederick Phillips Brooks").containsValues(Collections.singletonList(mythicalManMonth));
         assertThat(booksByAuthor).containsKey("Robert C. Martin").containsValues(Collections.singletonList(cleanCode));
     }
-    @Nested
-    @DisplayName("Est vide")
-    class IsEmpty {
-        @Test
-        @DisplayName("Quand aucun livre n'y est ajouté")
-        public void emptyBookShelfWhenNoBookAdded() {
-            List<Book> books = shelf.books();
-            assertTrue(books.isEmpty(), () -> "BookShelf devrait être vide.");
-        }
-        @Test
-        @DisplayName("Quand add est appelé sans livres")
-        void emptyBookShelfWhenAddIsCalledWithoutBooks() {
-            shelf.add();
-            List<Book> books = shelf.books();
-            assertTrue(books.isEmpty(), () -> "BookShelf devrait être vide.");
-        }
-    }
 
-    @Nested
-    @DisplayName("Après avoir ajouté des livres")
-    class BooksAreAdded {
-        @Test
-        @DisplayName("Contient deux livres")
-        void bookshelfContainsTwoBooksWhenTwoBooksAdded() {
-            // Test case removed for brevity
-        }
-        @Test
-        @DisplayName("Renvoie au client une collection de livres immuable ")
-        void bookshelfIsImmutableForClient() {
-            // Test case removed for brevity
-        }
+
     }
 
 
-}
+
+
+
