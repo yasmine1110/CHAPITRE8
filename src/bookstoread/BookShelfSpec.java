b514a05b4c99f1a20a1792d76856397a0405714d
yasmine1110 <yasminemariamd@gmail.com>
2026-06-08 11:14:33 +0000

    implementation de la  methode nested

--- src/bookstoread/BookShelfSpec.java
+++ src/bookstoread/BookShelfSpec.java
@@ -11,6 +11,7 @@ import java.util.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class BookShelfSpec {
@@ -135,6 +136,38 @@ public class BookShelfSpec {
         assertThat(booksByAuthor).containsKey("Steve McConnel").containsValues(Collections.singletonList(codeComplete));
         assertThat(booksByAuthor).containsKey("Frederick Phillips Brooks").containsValues(Collections.singletonList(mythicalManMonth));
         assertThat(booksByAuthor).containsKey("Robert C. Martin").containsValues(Collections.singletonList(cleanCode));
+    }
+    @Nested
+    @DisplayName("Est vide")
+    class IsEmpty {
+        @Test
+        @DisplayName("Quand aucun livre n'y est ajouté")
+        public void emptyBookShelfWhenNoBookAdded() {
+            List<Book> books = shelf.books();
+            assertTrue(books.isEmpty(), () -> "BookShelf devrait être vide.");
+        }
+        @Test
+        @DisplayName("Quand add est appelé sans livres")
+        void emptyBookShelfWhenAddIsCalledWithoutBooks() {
+            shelf.add();
+            List<Book> books = shelf.books();
+            assertTrue(books.isEmpty(), () -> "BookShelf devrait être vide.");
+        }
+    }
+
+    @Nested
+    @DisplayName("Après avoir ajouté des livres")
+    class BooksAreAdded {
+        @Test
+        @DisplayName("Contient deux livres")
+        void bookshelfContainsTwoBooksWhenTwoBooksAdded() {
+            // Test case removed for brevity
+        }
+        @Test
+        @DisplayName("Renvoie au client une collection de livres immuable ")
+        void bookshelfIsImmutableForClient() {
+            // Test case removed for brevity
+        }
     }
 
 
